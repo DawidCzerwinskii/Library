51982840067a6ba6de7dd43a78052522ae5c59b0
DawidCzerwinskii <czerwinskidawid95@gmail.com>
2024-09-29 12:00:22 +0200

    first commit

---
+++ library/src/Library.java
@@ -0,0 +1,21 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka b0.1";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseYear = 1911;
+        int pages = 320;
+        String publisher = "Wydawnictwo Greg";
+        String isbn = "978-83-66462-81-6";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece: ");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseYear);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
